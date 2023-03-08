c17d7a511382e57ed273725350f99eb62824fa59
Camila Ikkert <camilaikkert@gmail.com>
2023-03-07 23:57:14 -0300

    commit02

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 const btn = document.querySelector("[data-form-btn]");
 
-
+console.log(btn);
 
 /* EVENTO LISTENER */
 
-btn.addEventListener("click", function(){
-    console.log(btn);
-})
+/* btn.addEventListener("click", function(evento){
+    evento.preventDefault();
+    const input = document.querySelector("[data-form-input]");
+    console.log(input.value);
+}); */
+
+/* CONVERTIR FUNCION EN ARROW FUNCTION O FUNCION FLECHA*/
+
+/* btn.addEventListener("click", (evento) => {
+    evento.preventDefault();
+    const input = document.querySelector("[data-form-input]");
+    console.log(input.value);
+}); */
+
+/* CONVERTIT A FUNCION ANONIMA */
+
+const createTask = (evento) => {
+    evento.preventDefault();
+    const input = document.querySelector("[data-form-input]");
+    console.log(input.value);
+};
+
+btn.addEventListener("click", createTask);
+
+
